c7ccff636b90f3801d7b4936107c6bcf691c2f29
huntj88 <huntj88@gmail.com>
2020-11-21 15:44:56 -0600

    Using official iOS main thread Dispatcher

--- src/iosMain/kotlin/me/jameshunt/navfsm/IosFSMManager.kt
+++ src/iosMain/kotlin/me/jameshunt/navfsm/IosFSMManager.kt
@@ -1,11 +1,12 @@
 package me.jameshunt.navfsm
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 
 object IosCoroutine {
     private val job = Job()
-    val scope = CoroutineScope(job + MainLoopDispatcher)
+    val scope = CoroutineScope(job + Dispatchers.Main)
 }
 
 fun iosConfigure(uiRegistry: UIRegistry, getInitialFlow: () -> FSM<Unit, Unit>) {
